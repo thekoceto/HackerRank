307794fb722f2367ba3f6e73ed93a7fe5881b9e9
Kostadin Kostadinov <thekoceto@users.noreply.github.com>
2019-09-11 23:56:39 +0300

    Create InputAndOutput.cpp

---
+++ c++/Introduction/InputAndOutput.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int num1, num2, num3;
+    std::cin >> num1 >> num2 >> num3;
+    std::cout  << (num1 + num2 + num3)  << "\n";
+    return 0;
+}
